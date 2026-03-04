87e98c4ac517f8816d155caea100d154a14846ee
Leonard0CL <erickleon1026@gmail.com>
2026-03-04 15:24:21 -0500

    Add sensor, camera, translation, and children dictionaries

--- Corte1/Tarea/Corte1/Dictionaries1.py
+++ Corte1/Tarea/Corte1/Dictionaries1.py
@@ -1,1 +1,14 @@
+sensors ={"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
+num_cameras = {"backyard": 6, "garage": 2, "driveway": 1}
 
+print(sensors)
+print( num _cameras)
+translations = {"mountain": "orod", "bread": "bass", "friend": "mellon", "horse": "roch" }
+print (translations)
+
+#Verifiying an error:
+#powers = {[1, 2, 4, 8, 16]: 2, [1, 3, 9, 27, 81]: 3}
+#print (powers)
+
+children = {"von trapp": ["Johannes", "Rosmarie", "Eleonore"] , "Corleone": ["Sonny", "Fredo", "Michael"]}
+print(children)
